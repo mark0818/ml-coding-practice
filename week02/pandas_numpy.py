d62bbf358a2d929a018b9626df40b75b4da60c40
유병욱 <markyoo0818@gmail.com>
2026-03-22 13:56:52 +0900

    Auto commit: 2026-03-22 13:56 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -87,4 +87,8 @@ print(sample_df.agg(['count', 'mean', 'std', 'min', 'max']))
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/iris.csv'
 iris = pd.read_csv(file_url)
 
-print(iris.head)
+print(iris.head())
+
+print(iris.groupby('class').std())
+
+print(iris.drop('class', axis=1))
