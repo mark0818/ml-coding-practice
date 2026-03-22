fbdfef7a1aab3a77487aa1486a2fc90477392f3d
유병욱 <markyoo0818@gmail.com>
2026-03-22 13:23:05 +0900

    Auto commit: 2026-03-22 13:23 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -49,4 +49,5 @@ print(netflix['release_year'] > 2015)
 more2015 = netflix[netflix['release_year'] > 2015]
 print(more2015.head(10))
 
-print(~(netflix['release_year']))
+print(~(netflix['release_year'] > 2015))
+less2015 = netflix
