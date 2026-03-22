07454db6c2cd5fe6972c7ee9eb8b9313403f4bda
유병욱 <markyoo0818@gmail.com>
2026-03-22 13:56:03 +0900

    Auto commit: 2026-03-22 13:56 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -84,4 +84,7 @@ print(sample_df.describe())
 print(sample_df.std())
 print(sample_df.agg(['count', 'mean', 'std', 'min', 'max']))
 
+file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/iris.csv'
+iris = pd.read_csv(file_url)
 
+print(iris.head)
