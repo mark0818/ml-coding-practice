8676ec3bb04686fe6efc37cf8b7b92c4fb9d53d4
유병욱 <markyoo0818@gmail.com>
2026-03-22 13:10:35 +0900

    Auto commit: 2026-03-22 13:10 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -36,8 +36,8 @@ print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:3, 2:4])
 
-print(sample_df.drop(["var_1, var_3"], axis=1))
-print(sample_df.drop(["var_1, var_2"], axis=1))
+print(sample_df.drop(['var_1, var_3'], axis=1))
+print(sample_df.drop(['var_1, var_2'], axis=1))
 print(sample_df.drop(['a','b','c'], axis=0))
 
 netflix= pd.read_csv('2.1.1.netflix.csv')
