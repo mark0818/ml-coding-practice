0ba2ad05443a76571bb9f72e51ac6000ef182b22
유병욱 <markyoo0818@gmail.com>
2026-03-28 12:22:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -1,3 +1,9 @@
 import pandas as pd
+import os
 
 titanic = pd.read_csv('3.1.1.titanic.csv')
+# Get the directory where the script is located
+base_path = os.path.dirname(__file__)
+file_path = os.path.join(base_path, '3.1.1.titanic.csv')
+
+titanic = pd.read_csv(file_path)
