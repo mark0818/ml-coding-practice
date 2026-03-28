9e70decc33fa058aad21c7f496b7e6b1ca2de392
유병욱 <markyoo0818@gmail.com>
2026-03-28 13:10:29 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -122,10 +122,20 @@ print(survived_counts)
 
 plt.figure(figsize=(10, 6))
 
-plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
+plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
                  color='purple', alpha=0.9, label='Survived')
 
-plt.fill_between(survived_counts.index.astype(str), survived_counts[
+plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
+                 color='hotpink', alpha=0.6, label='Not Survived')
+
+plt.title('Survival by Age Group on Titanic')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+plt.legend()
+plt.grid(True)
+plt.savefig('Figure08.png')
+plt.close()
 
 
 
