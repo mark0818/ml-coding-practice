d521bbf211981282092c2973fd20f3a2a4b49785
유병욱 <markyoo0818@gmail.com>
2026-03-28 13:44:59 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -301,7 +301,7 @@ plt.hist(titanic['Age'], bins=20, color='skyblue', edgecolor='black')
 plt.xlabel('Age')
 plt.ylabel('Count')
 plt.title('Distribution of Ages on the Titanic')
-plt.grid(axis='y', linstyle='--', alpha=0.7)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 plt.savefig('Figure17.png')
 plt.close()
