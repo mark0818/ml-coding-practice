5f5373a0917caa4ec4ad1e4f79357e2efddc0e83
유병욱 <markyoo0818@gmail.com>
2026-03-28 13:22:59 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -183,3 +183,10 @@ plt.figure(figsize=(10,6))
 plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='o',
              capsize=5, capthick=1, label='Fare')
 
+plt.tite('Error Bar Plot of Fare by Parch')
+plt.xlabel('Parch')
+plt.ylabel('Fare')
+plt.xticks(fare_means.index)
+plt.legend()
+plt.savefig('Figure11.png')
+plt.close()
