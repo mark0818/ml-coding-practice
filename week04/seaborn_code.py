9fbb6031c799fb1440d0bf608ac264de1408e4fa
유병욱 <markyoo0818@gmail.com>
2026-04-03 13:03:45 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -8,3 +8,12 @@ tips = sns.load_dataset('tips')
 print(tips.head())
 
 tips.info()
+
+# **범주형 변수 산점도 그래프**
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+
