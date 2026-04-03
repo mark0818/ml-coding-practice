b380b7df4cb61be59390d54a9bc5d26b7c570b27
유병욱 <markyoo0818@gmail.com>
2026-04-03 14:28:31 +0900

    Auto commit: 2026-04-03 14:28 - 저장됨

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -6,3 +6,16 @@ import pandas as pd
 
 housing = pd.read_csv('housing.csv')       # -*- coding: utf-8 -*-
 
+# 데이터 준비
+
+#테스트 세트 만들기
+from sklearn.model_selection import train_test_split
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
+strat_train_set, strat_test_set = train_test_split(
+    housing, test_size=0.2, stratify=housing["income_cat"], random_state=42)
+
+
