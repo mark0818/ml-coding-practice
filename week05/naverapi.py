6ad17c41d48496cf0e2516769c1ee7ce8e35e5e6
유병욱 <markyoo0818@gmail.com>
2026-04-17 20:56:43 +0900

    Auto commit: 2026-04-17 20:56 - 저장됨

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -18,3 +18,5 @@ def main():
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
+            getPostData(post, jsonResult, cnt)                # [CODE 3]
+            
