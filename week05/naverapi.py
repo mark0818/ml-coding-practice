1ebe92db4ad1562a97f5038605934070529df985
유병욱 <markyoo0818@gmail.com>
2026-04-17 21:00:19 +0900

    Auto commit: 2026-04-17 21:00 - 저장됨

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -33,3 +33,13 @@ def main():
 
     print("가져온 데이터 : %d 건"%(cnt))
     print('%s_naver_%s.json SAVED' % (srcText, node))
+
+    def getNaverSearch(node, srcText, page_start, display):
+        base = "https://openapi.naver.com/v1/search"
+        node = "/%s.json" % node"
+        parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
+
+        url = base + node + parameters
+        responseDecode = getRequestUrl(url)               #[CODE 1]
+
+        if (response
