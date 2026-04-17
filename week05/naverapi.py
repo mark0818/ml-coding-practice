21c1d18e185a6586e26f7590eb1f396124277c9d
유병욱 <markyoo0818@gmail.com>
2026-04-17 20:45:35 +0900

    Auto commit: 2026-04-17 20:45 - 저장됨

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -2,3 +2,21 @@
 import urllib.request
 import datetime
 import json
+
+client_id = 'nvBig'
+client_secret = '08otyPMV_jikZ_HjUshk'
+
+def main():
+    node = 'news'                                             # 크롤링할 대상
+    srcText = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult= []
+
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
+    total = jsonResponse['total']
+
+while ((jsonResponse != None) and jsonResponse['display'] != 0):
+    for post in jsonResponse['items']:
+        cnt += 1
+        getPostData(post, jsonResult, cnt)
