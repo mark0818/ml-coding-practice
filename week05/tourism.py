835107d05ab5e484f703ecfcfa1fb739be3e482d
유병욱 <markyoo0818@gmail.com>
2026-04-17 21:21:10 +0900

    Auto commit: 2026-04-17 21:21 - 저장됨

--- week05/tourism.py
+++ week05/tourism.py
@@ -25,4 +25,11 @@ def main():
     result_df = pd.DataFrame(result, columns = columns)
     result_df.to_csv('./%s_%s_%d_%s.csv' % (nat_cd, ed_cd, nStartYear, dataEND), index = False, encoding = 'cp949')
 
-    
+"""### [CODE 3]"""    
+
+def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
+    jsonResult = []
+    result = []
+
+    for year in range(nStartYear, nEndYear+1):
+        
