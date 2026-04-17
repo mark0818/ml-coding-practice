51888abd74f84bdf364e5beb3d44d814033225cb
유병욱 <markyoo0818@gmail.com>
2026-04-17 21:48:20 +0900

    Auto commit: 2026-04-17 21:48 - 저장됨

--- week05/tourism.py
+++ week05/tourism.py
@@ -57,5 +57,8 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 
 """### [CODE 2]"""
 
-
+def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
+    service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
+    parameters = "?_type=json&serviceKey=" + ServiceKey       # 인증키
+    parameters += "&YM=" + y
 
