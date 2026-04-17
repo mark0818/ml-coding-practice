1d1f329e587594a9917f9974e2af5444ca6d2943
유병욱 <markyoo0818@gmail.com>
2026-04-17 20:43:31 +0900

    Auto commit: 2026-04-17 20:43 - 저장됨

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -3,8 +3,8 @@ import urllib.request
 import datetime
 import json
 
-client_id = 'Client ID'
-client_secret = 'Client Secret'
+client_id = 'nvBig'
+client_secret = ''
 
 def main():
     node = 'news'                                             # 크롤링할 대상
