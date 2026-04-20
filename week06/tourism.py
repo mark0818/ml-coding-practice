7a9a630cd37fc6224aaea1edc59811984088b41b
유병욱 <markyoo0818@gmail.com>
2026-04-20 21:29:34 +0900

    Auto commit: 2026-04-20 21:29 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -52,7 +52,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
                 jsonresult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
                 result.append([natName, nat_cd, yyyymm, num])
 
-    return (jsonresult, result, natName, ed)
+    return (jsonResult, result, natName, ed)
 
 """### [CODE 2]"""
 
