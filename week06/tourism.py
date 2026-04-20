5a9ab77922145a7d77819f62b7ba1b31ceaffe47
유병욱 <markyoo0818@gmail.com>
2026-04-20 21:25:28 +0900

    Auto commit: 2026-04-20 21:25 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -33,4 +33,6 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
     for year in range(nStartYear, nEndYear+1):
         for month in range(1, 13):
             yyyymm = "{0}{1:0>2}".format(str(year), str(month))
-            
+            jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)     #[CODE 2]
+            if (jsonData['response']['header']['resultMsg'] == 'OK'):
+                
