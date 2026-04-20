94680efd6e22413048788274611857256591af54
유병욱 <markyoo0818@gmail.com>
2026-04-20 20:55:35 +0900

    Auto commit: 2026-04-20 20:55 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -78,7 +78,11 @@ def getPostData(post, jsonResult, cnt):  #[CODE 3]
     jsonResult.append({
         'cnt': cnt,
         'title': title,
-        
+        'description': description,
+        'org_link': org_link,
+        'link': link,
+        'pDate': pDate
+    '
     })
 
 
