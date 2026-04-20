8faa0d2dc3e5102fb359c9b95749f1c7050115d2
유병욱 <markyoo0818@gmail.com>
2026-04-20 20:45:47 +0900

    Auto commit: 2026-04-20 20:45 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -3,4 +3,16 @@ import urllib.request
 import datetime
 import json
 
-client_id = 
+client_id = '08otyPMV_jikZ_HjUshk'
+client_secret = 'nRo5jC_WHa'
+
+def main():
+    node = 'news'
+    srcText = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)    # [CODE 2]
+    total = jsonResponse['total']
+    
