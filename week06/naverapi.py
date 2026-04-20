82ccd346baf377dd06cc09b99f5c6a390b37252e
유병욱 <markyoo0818@gmail.com>
2026-04-20 20:51:59 +0900

    Auto commit: 2026-04-20 20:51 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -50,6 +50,7 @@ def getNaverSearch(node, srcText, page_start, display):
 
 def getRequestUrl(url):
     req = urllib.request.Request(url)
+    req.add_header("X-Naver-Client-Id", client_id)
     
 
 
