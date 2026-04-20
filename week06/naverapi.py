21322179dd34efd104124929886418cc3c22727e
유병욱 <markyoo0818@gmail.com>
2026-04-20 20:51:16 +0900

    Auto commit: 2026-04-20 20:51 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -38,6 +38,16 @@ def main():
 def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
-    parameters = 
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
+
+    url = base + node + parameters
+    responseDecode = getRequestUrl(url)                #[CODE 1]
+
+    if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
+
+
 
 
