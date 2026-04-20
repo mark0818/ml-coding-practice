ca22eed3e1a9dce4aba50f982fe2feb1941779ff
유병욱 <markyoo0818@gmail.com>
2026-04-20 20:53:31 +0900

    Auto commit: 2026-04-20 20:53 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -55,6 +55,14 @@ def getRequestUrl(url):
 
     try:
         response = urllib.request.urlopen(req)
-        if response
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
+    
+
 
 
