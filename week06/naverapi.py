29a2fdeabb57ffadabb36af1a55832b30226ab59
유병욱 <markyoo0818@gmail.com>
2026-04-20 21:11:06 +0900

    Auto commit: 2026-04-20 21:11 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -28,7 +28,7 @@ def main():
     print('전체 검색 : %d 건' %total)
 
     with open('%s_naver_%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
-        jsonFile = json.dumps(jsonResult, indent=ent = 4, sort_keys = True,
+        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
                                 ensure_ascii = False)
 
         outfile.write(jsonFile)
