559534b97e499ae4ad5b9e27c94f92168d03690b
유병욱 <markyoo0818@gmail.com>
2026-05-05 20:39:48 +0900

    Auto commit: 2026-05-05 20:39 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -181,4 +181,5 @@ netflix_age_country_unstack
 plt.figure(figsize=(15, 5))
 
 # 사용자 정의 컬러맵 만들기
-cmap = plt.matplotlib.colors.LinearSegmentedColormap.from
+cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710','#f5f5f1'])
+
