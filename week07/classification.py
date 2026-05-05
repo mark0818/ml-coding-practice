4c1267b96f7b181796907d5efcb32441deddc96f
유병욱 <markyoo0818@gmail.com>
2026-05-05 19:51:15 +0900

    Auto commit: 2026-05-05 19:51 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -66,4 +66,15 @@ y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
 from sklearn.metrics import confusion_matrix
 
+cm = confusion_matrix(y_train_5, y_train_pred)
+print(cm)
 
+y_train_perfect_predictions = y_train_5  # 완벽한 분류기일 경우
+print(confusion_matrix(y_train_5, y_train_perfect_predictions))
+
+#정밀도와 재현율
+from sklearn.metrics import precision_score, recall_score
+
+print(precision_score(y_train_5, y_train_pred))  # == 3530 / (687 + 3530)
+
+print(recall_score(y_train
