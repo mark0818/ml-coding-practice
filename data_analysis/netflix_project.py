37b6103ac4236ee9a424c7de4c94252ee623af72
유병욱 <markyoo0818@gmail.com>
2026-05-05 20:28:59 +0900

    Auto commit: 2026-05-05 20:28 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -98,4 +98,5 @@ netflix['title'].str.contains('squid game', na=False, case=False)
 # 오징어 게임을 검색한 조건을 넷플릭스 데이터에 넣어서 True인 값만 출력
 netflix[netflix['title'].str.contains('Squid Game', na=False, case=False)]
 
-type_counts = 
+type_counts = netflix['type'].value_counts()
+print(type_counts)
