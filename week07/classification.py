f4c2c01f24e871977c6ecb93f26073cc158764be
유병욱 <markyoo0818@gmail.com>
2026-05-05 19:46:16 +0900

    Auto commit: 2026-05-05 19:46 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -40,4 +40,9 @@ y_test_5 = (y_test == '5')
 
 from sklearn.linear_model import SGDClassifier
 
-sgd_clf = SGDCClassifier(rea)
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
+
+#성능 측정 - 교차 검증을 사용한 정확도 측정
