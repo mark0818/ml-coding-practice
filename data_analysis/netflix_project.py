1c71cc3e6d297eb194bb3390472f8579fb29a996
유병욱 <markyoo0818@gmail.com>
2026-05-05 20:32:11 +0900

    Auto commit: 2026-05-05 20:32 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -123,4 +123,7 @@ netflix['listed_in'].str.split(', ', expend=True)
 
 # [2단계] .stack( )을 사용하면 여러 열로 구성한 데이터프레임을 1개의 열로 만들어 쌓음
 # 예시) 인덱스 1의 listed_in 열 값 : International TV Shows, TV Dramas, TV Mysteries
-netflix['listed_in']
+netflix['listed_in'].str.split(', ', expand=True).stack()
+
+# [3단계] .value_counts( )를 붙여 장르의 등장 횟수 계산
+
