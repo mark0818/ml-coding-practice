37ee9f7534165e02cae673d59bec6409115b23e0
유병욱 <markyoo0818@gmail.com>
2026-05-05 20:33:23 +0900

    Auto commit: 2026-05-05 20:33 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -127,4 +127,5 @@ netflix['listed_in'].str.split(', ', expand=True).stack()
 
 # [3단계] .value_counts( )를 붙여 장르의 등장 횟수 계산
 # 예시) 인덱스 1의 listed_in 열 값 : International TV Shows, TV Dramas, TV Mysteries
-genres = netflix['listed_in']
+genres = netflix['listed_in'].str.split(', ', expand=True).stack().value_counts()
+genres
