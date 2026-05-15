02be4aa6b9f9f375f23c0a358767a055a857b972
유병욱 <markyoo0818@gmail.com>
2026-05-15 22:39:31 +0900

    Auto commit: 2026-05-15 22:39 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -38,3 +38,6 @@ ploynomial_svm_clf.fit(X, y)
 
 from sklearn.svm import SVC
 
+ploynomial_svm_clf = make_pipeline(StandardScaler(),
+                                   SVC(kernel="poly", degree=3, coef0=1, C=5))
+poly_kernel_svm_clf.fit(X, y)
