f69711a38959291940959d0d3165c0255d1e793e
유병욱 <markyoo0818@gmail.com>
2026-05-15 21:56:50 +0900

    Auto commit: 2026-05-15 21:56 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
-import
+import matplotlib.pyplot as plt
+
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
