976536a0ad33eb90f66e6ab93a242757e71f6e73
유병욱 <markyoo0818@gmail.com>
2026-05-15 22:02:19 +0900

    Auto commit: 2026-05-15 22:02 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -37,5 +37,7 @@ print(lin_reg.predict(X_new))
 
 from sklearn.linear_model import SGDRegressor
 
-sdg_reg = SGDRegressor(max_iter=1000, tol=1e-5, penalty=None, eta0=0.01)
+sdg_reg = SGDRegressor(max_iter=1000, tol=1e-5, penalty=None, eta0=0.01,
+                       n_iter_no_change=100, random_state=42)
+                       )
 
