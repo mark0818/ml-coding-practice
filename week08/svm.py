e601045bc086201cd8372b19b3a0872c3c10b26b
유병욱 <markyoo0818@gmail.com>
2026-05-15 22:37:22 +0900

    Auto commit: 2026-05-15 22:37 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -27,4 +27,8 @@ from sklearn.preprocessing import PolynomialFeatures
 
 X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
 
-ploynomial_svm_clf = make_pipeline(PolynomialFeatures(degree=3),
+ploynomial_svm_clf = make_pipeline(
+    PolynomialFeatures(degree=3),
+    StandardScaler(),
+    LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
+    
