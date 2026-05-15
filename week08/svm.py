46a839d442d3f6b6e27baf45c34662162d29479d
유병욱 <markyoo0818@gmail.com>
2026-05-15 22:43:58 +0900

    Auto commit: 2026-05-15 22:43 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -67,5 +67,7 @@ np.random.seed(42)
 X = 2 * np.random.rand(50, 1) - 1
 y = 0.2 + 0.1 * X[:, 0] + 0.5 * X[:, 0] ** 2 + np.random.randn(50) / 10
 
+svm_poly_reg = make_pipeline(StandardScaler(),
+                             SVR(kernel="poly", degree=2, C=0.01, epsilon=0.1))
 
 
