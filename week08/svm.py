4b555d2ebae2ca420d420b5b89503b5ee007929a
유병욱 <markyoo0818@gmail.com>
2026-05-15 22:40:49 +0900

    Auto commit: 2026-05-15 22:40 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -27,12 +27,12 @@ from sklearn.preprocessing import PolynomialFeatures
 
 X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
 
-ploynomial_svm_clf = make_pipeline(
+polynomial_svm_clf = make_pipeline(
     PolynomialFeatures(degree=3),
     StandardScaler(),
     LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
 )
-ploynomial_svm_clf.fit(X, y)
+polynomial_svm_clf.fit(X, y)
 
 # 다항식 커널
 
