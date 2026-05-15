6bffc53c91491fd5c8c6408e0191c3e308475bba
유병욱 <markyoo0818@gmail.com>
2026-05-15 22:33:39 +0900

    Auto commit: 2026-05-15 22:33 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -7,3 +7,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
+iris = load_iris(as_frame=True)
+X = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y = (iris.target == 2)  # Iris virginica
