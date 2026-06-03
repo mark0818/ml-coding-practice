36544cfa3087fb33f1b578dcff67ea0ac8552c6c
유병욱 <markyoo0818@gmail.com>
2026-06-03 14:48:34 +0900

    Auto commit: 2026-06-03 14:48 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -22,7 +22,7 @@ X, y = make_blobs(n_samples=2000, centers=blob_centers, cluster_std=blob_std,
                   random_state=7)
 
 k = 5
-kmeans = KMeans = KMeans(n_clusters=k, n_init=10, random_state=42)
+kmeans = KMeans(n_clusters=k, n_init=10, random_state=42)
 y_pred = kmeans.fit_predict(X)
 
 def plot_clusters(X, y=None):
