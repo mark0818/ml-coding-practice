c265283d05ed766ed1a295f00fd6bca0b4d43d85
유병욱 <markyoo0818@gmail.com>
2026-06-03 14:35:34 +0900

    Auto commit: 2026-06-03 14:35 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -135,4 +135,8 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     anomalies_mask = dbscan.labels_ == -1
     non_core_mask = ~(core_mask | anomalies_mask)
 
+    cores = dbscan.components_
+    anomalies = X[anomalies_mask]
+    non_cores = X[non_core_mask]
 
+    
