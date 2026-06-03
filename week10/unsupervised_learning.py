1ca002e8bfa64e5591a15cf7562f8ee54e3f2ab5
유병욱 <markyoo0818@gmail.com>
2026-06-03 14:32:21 +0900

    Auto commit: 2026-06-03 14:32 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -124,4 +124,7 @@ X, y = make_moons(n_samples=1000, noise=0.05, random_state=42)
 dbscan = DBSCAN(eps=0.05, min_samples=5)
 dbscan.fit(X)
 
+print(dbscan.labels_[:10])
+
+print(dbscan.core_sample_indices_[:10])
 
