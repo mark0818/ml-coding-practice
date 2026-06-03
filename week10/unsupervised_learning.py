55dd9645f01744753400ba6d9a42693870b2a315
유병욱 <markyoo0818@gmail.com>
2026-06-03 14:42:01 +0900

    Auto commit: 2026-06-03 14:42 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -158,4 +158,12 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
 dbscan2 = DBSCAN(eps=0.2)
 dbscan2.fit(X)
 
-plt.
+plt.figure(figsize=(9, 3.2))
+
+plt.subplot(121)
+plot_dbscan(dbscan, X, size=100)
+
+plt.subplot(122)
+plot_dbscan(dbscan2, X, size=600, show_ylabels=False)
+
+
