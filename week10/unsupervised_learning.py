6f5bf0b7447ed35d23a3f14dcc1150f349f3bb8f
유병욱 <markyoo0818@gmail.com>
2026-06-03 14:43:04 +0900

    Auto commit: 2026-06-03 14:43 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -175,4 +175,8 @@ dbscan = DBSCAN(eps=0.2, min_samples=5)
 dbscan.fit(X)
 
 knn = KNeighborsClassifier(n_neighbors=50)
-knn
+knn.fit(dbscan.components_, dbscan.labels_[dbscan.core_sample_indices_])
+
+X_new = np.array([[-0.5, 0], [0, 0.5], [1, -0.1], [2, 1]])
+print(knn.predict(X_new))
+print(knn.predict_proba(X_new))
