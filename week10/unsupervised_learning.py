f4a33c21246da9e6755d5cc4f3afeb2d8218db79
유병욱 <markyoo0818@gmail.com>
2026-06-03 14:21:25 +0900

    Auto commit: 2026-06-03 14:21 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -53,7 +53,11 @@ kmeans_per_k = [KMeans(n_clusters=k, n_init=10, random_state=42).fit(X) for k in
 inertias = [model.inertia_ for model in kmeans_per_k]
 
 plt.figure(figsize=(8, 3.5))
-plt.plot(range)
+plt.plot(range(1, 10), inertias, "bo-")
+plt.xlabel("$k$")
+plt.ylabel("이너셔")
+plt.annotate("", xy=(5, inertias[4]), xytext=(4.45, 650), arrowprops=dict(facecolor='black', shrink=0.1))
+
 
 
 
